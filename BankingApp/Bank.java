63639d9454a267a2fa634a5203a3e8941382ca41
cocotajle <alternative.lady@gmail.com>
2019-02-12 11:04:01 +0800

    Create Bank.java

---
+++ BankingApp/Bank.java
@@ -0,0 +1,83 @@
+package com.natjavaudemy;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if (findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomer(String branchName, String customerName, double initialAmount) {
+        Branch branch = findBranch(branchName);
+        if (branch != null) {
+            return branch.newCustomer(customerName, initialAmount);
+        }
+return false;
+
+    }
+
+    public boolean addCustomerTransaction(String branchName, String cusomerName, double amount){
+
+        Branch branch = findBranch(branchName);
+        if(branch != null){
+            return branch.addCustomerTransaction(cusomerName,amount);
+        }
+        return false;
+
+    }
+    private Branch findBranch(String branhName){
+        for(int i = 0 ; i< this.branches.size();i++){
+            Branch checkedBranch = this.branches.get(i);
+            if(checkedBranch.get().equals(branhName)){
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+}
