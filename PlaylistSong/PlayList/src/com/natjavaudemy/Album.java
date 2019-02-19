4a3329929eeddb7d07540157b6df7988b1d385ae
cocotajle <alternative.lady@gmail.com>
2019-02-20 06:05:57 +0800

    up

--- PlaylistSong/PlayList/src/com/natjavaudemy/Album.java
+++ PlaylistSong/PlayList/src/com/natjavaudemy/Album.java
@@ -13,22 +13,52 @@ public class Album {
         this.artistName = artistName;
         this.albumSongsList = new ArrayList<Song>();
     }
-    public boolean addSong(String title, double duration){
-        if (finfSong(title) == null){
-            this.albumSongsList.add(new Song(title,duration));
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null) {
+            this.albumSongsList.add(new Song(title, duration));
             return true;
-        }return false;
+        }
+        return false;
     }
-    public Song finfSong(String title){
+/*    public Song finfSong(String title){
             for (int i = 0; i < this.albumSongsList.size(); i++) {
                 Song checkedSong = this.albumSongsList.get(i);
                 if (checkedSong.getSongTitle().equals(title)) {
                     return checkedSong;
                 }
             }
-            return null;
+            return null;*/
+
+    private Song findSong(String title) {
+        for (Song chceckedSong : this.albumSongsList) {
+            if (chceckedSong.getSongTitle().equals(title)) {
+                return chceckedSong;
+            }
+        }
+        return null;
     }
 
+    public boolean addToPlaylist(int tracknumber, LinkedList<Song> playList) {
+
+        int index = tracknumber - 1;
+        ((index >= 00 && (index <= this.albumSongsList.size()))) {
+            playList.add(this.albumSongsList.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track");
+        return false;
+    }
+
+    public boolean addToPlaylist (String titile, LinkedList<Song> playlist){
+        Song chceckedSong = findSong(titile);
+        if(chceckedSong != null){
+            playlist.add(chceckedSong);
+            return true;
+        }
+        System.out.println("This song " + titile + " is not in this album.");
+        return  false;
+    }
 
 
 }
